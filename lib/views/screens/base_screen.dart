b26662d94848f67ba86672b9a334b4bc1bdf652f
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-08-10 20:38:04 +0530

    bottom nav widget created

--- lib/views/screens/base_screen.dart
+++ lib/views/screens/base_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:walkmate/views/shared/style.dart';
+
+import '../shared/bottom_navbar.dart';
 
 class BaseScreen extends StatelessWidget {
   const BaseScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(size.width * 0.03),
+          margin: EdgeInsets.symmetric(
+              horizontal: size.width * 0.03, vertical: size.height * 0.03),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [BottomNavWidget(size: size)],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'base',
+          style: textStyle(40, Colors.black, FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
